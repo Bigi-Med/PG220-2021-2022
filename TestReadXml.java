7d93b8091852ce20f9bd46a9ae954d37429e8bab
Mohamed Bigi <Mohamed.Bigi@bordeaux-inp.fr>
2021-11-15 12:21:35 +0100

    read xml

--- TestReadXml.java
+++ TestReadXml.java
@@ -1,8 +1,10 @@
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamConstants;
 import java.io.FileInputStream;
 import java.io.IOException;
+
 
 public class TestReadXml {
 
@@ -13,17 +15,35 @@ public class TestReadXml {
             file = new FileInputStream("clients.xml");
             XMLInputFactory xmlInFact = XMLInputFactory.newInstance();
             XMLStreamReader reader = xmlInFact.createXMLStreamReader(file);
-            if(reader.hasNext()) {
+            int event = reader.getEventType();
+            System.out.println(XMLStreamConstants.START_ELEMENT );
+            while(reader.hasNext()) {
+              reader.next(); // On regarde la liste des clients
+              if(event == XMLStreamConstants.START_DOCUMENT || event == XMLStreamConstants.START_ELEMENT || event  == XMLStreamConstants.END_ELEMENT  )
+              {
 
-                reader.next(); // On regarde la liste des clients
+                
 
-                reader.nextTag(); //On regarde le premier client
+                //reader.nextTag(); //On regarde le premier client
                 reader.nextTag(); //On regarde la première commande de planche demandée
-
                 String commande = "Commande n°" + reader.getAttributeValue(0) + " de " + reader.getAttributeValue(1) + " planches à livrer pour le " + reader.getAttributeValue(2) + " au prix maximal de " +reader.getAttributeValue(3);
+                reader.nextTag();//dimension
+                reader.nextTag();//close dim
+                reader.nextTag();//close planche
+                reader.nextTag();//close  client
                 System.out.println(commande);
-            }
-        }
+                 event = reader.getEventType();
+                 System.out.println(event);
+               }
+               else{
+                 System.out.println("in break");
+                 // reader.close();
+                 // System.out.println(event);
+                 break;
+
+               }
+               }
+             }
         catch(IOException exc) {
 
             System.out.print("Erreur IO: " + exc.toString());
@@ -32,7 +52,5 @@ public class TestReadXml {
             System.out.print("Erreur XML: " + exc.toString());
 
         }
-    }
-
-
+      }
 }
