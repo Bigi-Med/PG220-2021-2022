0dc559df55c4fa3f722f608ebe097272e4a741af
Mohamed Bigi <Mohamed.Bigi@bordeaux-inp.fr>
2021-11-16 18:07:15 +0100

    reading xml files

--- testXML/Planche.java
+++ testXML/Planche.java
@@ -1,4 +1,14 @@
 public class Planche
 {
-
+  int id;
+  int nombre;
+  String date;
+  String price;
+  Planche(int id, int nombre,String date,String price)
+  {
+    this.id = id;
+    this.nombre = nombre;
+    this.date = date;
+    this.price = price;
+  }
 }
--- testXML/XMLReader.java
+++ testXML/XMLReader.java
@@ -9,6 +9,11 @@ import java.util.List;
 
 public class XMLReader
 {
+  static int check = 0;
+  static boolean value;
+  public static void main(String[] args) {
+    readXml("../clients.xml");
+  }
     static void readXml(String filename)
     {
         try
@@ -17,12 +22,25 @@ public class XMLReader
             XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(file);
             while(reader.hasNext())
             {
-                if(reader.next() == XMLStreamConstants.START_ELEMENT)
+              if(check < 3)
+              {  value =(reader.next() == XMLStreamConstants.START_ELEMENT);
+                check++;
+              }
+
+                if(value)
                 {
+                  value = true;
+
                     if(reader.getName().toString() == "client")
                     {
+
                         Client c = readClient(reader);
+                        System.out.println("CLient id : "+c.id);
+                        System.out.println("==============================================================================================");
+
                     }
+
+
                 }
             }
         }
@@ -38,24 +56,71 @@ public class XMLReader
     static Client readClient(XMLStreamReader reader) throws XMLStreamException
     {
         int id = Integer.parseInt(reader.getAttributeValue(0));
+
         List<Planche> listPlanche = new ArrayList<>();
         while(reader.hasNext())
         {
-            if(reader.next() == XMLStreamConstants.START_ELEMENT)
+          if(reader.next() != XMLStreamConstants.START_ELEMENT )
             {
-                if(reader.getName().toString() == "planche")
-                {
-                    Planche p = readPlanche(reader);
-                    listPlanche.add(p);
-                }
+              continue;
+            }
+            if(reader.getName().toString()=="planche")
+            {
+              Planche p = readPlanche(reader);
+              System.out.println("planche id " + p.id);
+              System.out.println("planche nombre "+p.nombre);
+              System.out.println("planche date  "+ p.date);
+              System.out.println("planche price  "+p.price);
+              System.out.println("============================");
+              listPlanche.add(p);
+              continue;
 
             }
+            else if (reader.getName().toString() == "client"){
+
+              break;
+
+            }
+
         }
+
         return new Client(id,listPlanche);
     }
 
     static Planche readPlanche(XMLStreamReader reader) throws XMLStreamException
     {
-        return null;
+
+      int id = Integer.parseInt(reader.getAttributeValue(0));
+      int nombre = Integer.parseInt(reader.getAttributeValue(1));
+      String date = reader.getAttributeValue(2);
+      String price = reader.getAttributeValue(3);
+      List<Dimension> listDimension = new ArrayList<>();
+      while(reader.hasNext())
+      {
+        if(reader.next() == XMLStreamConstants.START_ELEMENT)
+        {
+
+            if(reader.getName().toString() == "dim")
+            {
+            Dimension d = readDimension(reader);
+            System.out.println("dimension l " +d.l);
+            System.out.println("dimension L "+d.L);
+            System.out.println("============================");
+             break;
+          }
+
+
+        }
+
+      }
+
+
+        return new Planche(id,nombre,date,price);
+    }
+    static Dimension readDimension(XMLStreamReader reader) throws XMLStreamException
+    {
+      String L = reader.getAttributeValue(0);
+      String l = reader.getAttributeValue(1);
+      return new Dimension(L,l);
     }
 }
