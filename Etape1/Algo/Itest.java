231defe9646bc99b4a6f773f0dbf07d2d44493d3
Mohamed Bigi <Mohamed.Bigi@bordeaux-inp.fr>
2021-12-05 20:05:40 +0100

    adding tests to new main

--- Etape1/Algo/Itest.java
+++ Etape1/Algo/Itest.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.lang.Exception;
 import java.lang.SuppressWarnings;
+import java.util.Locale;
 
  class Itest{
 
@@ -45,25 +46,61 @@ import java.lang.SuppressWarnings;
          {
 
 
-            System.out.println( "Supplier with id  : " + u.id +" has : ");
+          System.out.println( "Supplier with id  : " + u.id +" has : ");
           for(Wood w : u.listWood )
           {
+            System.out.println("============PANEL===============");
+            System.out.println("");
             System.out.println("Panel with id : " + w.id + " and number : "+w.nombre + " with price : " +w.price + " with date : " + w.date + " with dimensions : ");
             for(Dimension d : w.listDim)
             {
-              System.out.println(" width of : " + d.l + " and length of : " + d.L);
-              System.out.println("");
+                System.out.println(" width of : " + d.l + " and length of : " + d.L);
+                System.out.println("");
+                System.out.println("==================CHEKING ATTRIBUTES====================");
+                System.out.println("");
+
+
+                if((w.isPrice(w.price)) && (w.isDimension(d)) && (w.isId((double)w.id)) && (w.isNumber(w.nombre) && w.isDate("dd.MM.yy",w.date,Locale.ENGLISH)))
+                {
+                  System.out.println(" CHECK OK");
+                  System.out.println("");
+                }
+                 if (w.isPrice(w.price) == false){
+                  System.out.println("PRICE INVALID !");
+                  System.out.println("");
+                }
+                 if(w.isDimension(d) == false){
+                  System.out.println("DIMENSION INVALID !");
+                  System.out.println("");
+                }
+                 if(w.isId((double)w.id)== false)
+                {
+                  System.out.println("ID INVALID !");
+                  System.out.println("");
+                }
+                 if(w.isNumber(w.nombre)== false)
+                {
+                  System.out.println("NOMBRE INVALID !");
+                  System.out.println("");
+                }
+                if(w.isDate("dd.MM.yy",w.date,Locale.ENGLISH) == false)
+                {
+                  System.out.println("DATE INVALID !");
+                  System.out.println("");
+                }
 
 
 
               }
             }
             System.out.println("=====================================================================================");
+            System.out.println("");
+
           }
 
           xml.ReadFile(files[0]);
           Client.ConstructObj(Iread.infos);
-          
+
 
           for(Client u : ClientFactory.ListClient)
           {
@@ -72,18 +109,85 @@ import java.lang.SuppressWarnings;
              System.out.println( "Client with id  : " + u.id +" has : ");
            for(Wood w : u.listWood )
            {
+             System.out.println("============PLANK===========");
+             System.out.println("");
+
+
              System.out.println("Plank with id : " + w.id + " and number : "+w.nombre + " with price : " +w.price + " with date : " + w.date + " with dimensions : ");
              for(Dimension d : w.listDim)
              {
                System.out.println(" width of : " + d.l + " and length of : " + d.L);
                System.out.println("");
+               System.out.println("==================CHEKING ATTRIBUTES====================");
+               System.out.println("");
+
+
+               if((w.isPrice(w.price)) && (w.isDimension(d)) && (w.isId((double)w.id)) &&   (w.isNumber(w.nombre) && w.isDate("dd.MM.yy",w.date,Locale.ENGLISH)))
+               {
+                 System.out.println(" CHECK OK");
+                 System.out.println("");
+               }
+                if (w.isPrice(w.price) == false){
+                 System.out.println("PRICE INVALID !");
+                 System.out.println("");
+               }
+
+                if(w.isDimension(d) == false){
+                 System.out.println("DIMENSION INVALID !");
+                 System.out.println("");
+               }
+                if(w.isId((double)w.id)== false)
+               {
+                 System.out.println("ID INVALID !");
+                 System.out.println("");
+               }
+                if(w.isNumber(w.nombre)== false)
+               {
+                 System.out.println("NOMBRE INVALID !");
+                 System.out.println("");
+               }
+               if(w.isDate("dd.MM.yy",w.date,Locale.ENGLISH) == false)
+               {
+                 System.out.println("DATE INVALID !");
+                 System.out.println("");
+               }
 
 
 
                }
              }
              System.out.println("=====================================================================================");
+             System.out.println("");
+
            }
 
         }
         }
+        // if((w.isPrice(w.price)) && (w.isDimension(d)) && (w.isId((double)w.id)) && (w.isNumber(w.nombre) && w.isDate("dd.MM.yy",w.date,Locale.ENGLISH)))
+        //       {
+        //         System.out.println(" CHECK OK");
+        //         System.out.println("");
+        //       }
+        //        if (w.isPrice(w.price) == false){
+        //         System.out.println("PRICE INVALID !");
+        //         System.out.println("");
+        //       }
+        //        if(w.isDimension(d) == false){
+        //         System.out.println("DIMENSION INVALID !");
+        //         System.out.println("");
+        //       }
+        //        if(w.isId((double)w.id)== false)
+        //       {
+        //         System.out.println("ID INVALID !");
+        //         System.out.println("");
+        //       }
+        //        if(w.isNumber(w.nombre)== false)
+        //       {
+        //         System.out.println("NOMBRE INVALID !");
+        //         System.out.println("");
+        //       }
+        //       if(w.isDate("dd.MM.yy",w.date,Locale.ENGLISH) == false)
+        //       {
+        //         System.out.println("DATE INVALID !");
+        //         System.out.println("");
+        //        //
